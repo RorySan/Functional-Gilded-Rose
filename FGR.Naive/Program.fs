55f07cc909eba47823ae6d0e8446f3c4caa920f4
RorySan <games@rory.es>
2025-05-18 18:22:55 +0200

    loops to match expression

--- FGR.Naive/Program.fs
+++ FGR.Naive/Program.fs
@@ -3,39 +3,51 @@ type Item =
     { Name: string
       SellIn: int
       Quality: int }
+    
+let maxQuality = 50
+let minQuality = 0
+let isExpired item = item.SellIn <= 0
+let expiresIn days item =
+    item.SellIn <= days  
 
+let increaseQuality amount item =
+    let newQuality = item.Quality + amount    
+    { item with Quality = min maxQuality newQuality } 
 
+let decreaseQuality amount item =
+    let newQuality = item.Quality - amount
+    { item with Quality = max minQuality newQuality }
+        
+let decreaseSellin item =
+    { item with SellIn = item.SellIn - 1 }
+        
 let updateQuality(items : array<Item>) =
-        for i = 0 to items.Length - 1 do
-            if items[i].Name <> "Aged Brie" && items[i].Name <> "Backstage passes to a TAFKAL80ETC concert" then
-                if items[i].Quality > 0 then
-                    if items[i].Name <> "Sulfuras, Hand of Ragnaros" then
-                        items[i] <- { items[i] with Quality = (items[i].Quality - 1) }
-            else
-               if items[i].Quality < 50 then
-                    items[i] <- { items[i] with Quality = (items[i].Quality + 1) }
-                    if items[i].Name = "Backstage passes to a TAFKAL80ETC concert" then
-                        if items[i].SellIn < 11 then
-                            if items[i].Quality < 50 then
-                                items[i] <- { items[i] with Quality = (items[i].Quality + 1) }
-                        if items[i].SellIn < 6 then
-                            if items[i].Quality < 50 then
-                                items[i] <- { items[i] with Quality = (items[i].Quality + 1) }
-            if items[i].Name <> "Sulfuras, Hand of Ragnaros" then
-                items[i] <- { items[i] with SellIn  = (items[i].SellIn - 1) }
-            if items[i].SellIn < 0 then
-                if items[i].Name <> "Aged Brie" then
-                    if items[i].Name <> "Backstage passes to a TAFKAL80ETC concert" then
-                        if items[i].Quality > 0 then
-                            if items[i].Name <> "Sulfuras, Hand of Ragnaros" then
-                                items[i] <- { items[i] with Quality   = (items[i].Quality  - 1) }
-                    else
-                        items[i] <- { items[i] with Quality   = (items[i].Quality  - items[i].Quality) }
-                else
-                    if items[i].Quality < 50 then
-                        items[i] <- { items[i] with Quality   = (items[i].Quality + 1) }
         items
-
+        |> Array.map (fun item ->
+            match item.Name with
+            | "Aged Brie" ->
+                let qualityIncrease =
+                    if isExpired item then 2 else 1
+                item
+                |> increaseQuality qualityIncrease
+                |> decreaseSellin         
+            | "Sulfuras, Hand of Ragnaros" -> item
+            | "Backstage passes to a TAFKAL80ETC concert" ->
+                if isExpired item then
+                    { item with Quality = 0; }
+                elif item.SellIn <= 5 then
+                    item |> increaseQuality 3 
+                elif item.SellIn <= 10 then
+                    item |> increaseQuality 2
+                else 
+                    item |> increaseQuality 1
+                |> decreaseSellin
+            | _ ->
+                if isExpired item then
+                   item |> decreaseQuality 2
+                else
+                    item |> decreaseQuality 1
+                |> decreaseSellin)
 
 module Program =
     [<EntryPoint>]
